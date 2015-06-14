aa35c55e465d2aace581c647fee84db04fda2337
Giuseppe Trisciuoglio <giuseppe.trisciuoglio@gmail.com>
2015-06-14 20:01:55 +0200

    add new version

--- build.sh
+++ build.sh
@@ -7,3 +7,5 @@ echo =========================================================================
 echo Docker image is ready.  Try it out by running:
 echo     docker run --rm -ti -P ${DOCKER_IMAGE_NAME}
 echo =========================================================================
+
+docker run -ti -p 8101:8101 -p 8181:8181 -d --name jboss-fuse jboss/jboss-fuse-full
